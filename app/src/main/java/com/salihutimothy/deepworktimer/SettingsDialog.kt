18215de436322fbec190e8e87aebdb70ecd147f1
DrTim105 <Salihutimothy52@gmail.com>
2022-12-27 03:58:50 +0100

    updated design: colors; updated settings_dialog.xml: added code for reading and saving values

--- app/src/main/java/com/salihutimothy/deepworktimer/SettingsDialog.kt
+++ app/src/main/java/com/salihutimothy/deepworktimer/SettingsDialog.kt
@@ -1,21 +1,38 @@
 package com.salihutimothy.deepworktimer
 
 import android.os.Bundle
+import android.preference.PreferenceManager.getDefaultSharedPreferences
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.SeekBar
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatDialogFragment
+import java.util.*
 
 private const val TAG = "SettingsDialog"
 
-class SettingsDialog: AppCompatDialogFragment() {
-    private lateinit var okButton : Button
-    private lateinit var cancelButton : Button
+const val SETTINGS_FIRST_DAY_OF_WEEK = "FirstDay"
+const val SETTINGS_IGNORE_LESS_THAN = "IgnoreLessThan"
+const val SETTINGS_DEFAULT_IGNORE_LESS_THAN = 0
 
+class SettingsDialog : AppCompatDialogFragment() {
+    private lateinit var okButton: Button
+    private lateinit var cancelButton: Button
+    private lateinit var firstDaySpinner: Spinner
+    private lateinit var ignoreSeconds: SeekBar
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    private val defaultFirstDayOfWeek = GregorianCalendar(Locale.getDefault()).firstDayOfWeek
+    private var firstDay = defaultFirstDayOfWeek
+    private var ignoreLessThan = SETTINGS_DEFAULT_IGNORE_LESS_THAN
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         Log.d(TAG, "onCreateView: called")
         return inflater.inflate(R.layout.settings_dialog, container, false)
     }
@@ -25,13 +42,52 @@ class SettingsDialog: AppCompatDialogFragment() {
 
         okButton = view.findViewById(R.id.okButton)
         cancelButton = view.findViewById(R.id.cancelButton)
+        firstDaySpinner = view.findViewById(R.id.firstDaySpinner)
+        ignoreSeconds = view.findViewById(R.id.ignoreSeconds)
+
 
         super.onViewCreated(view, savedInstanceState)
         okButton.setOnClickListener {
-            // saveValues()
+            saveValues()
             dismiss()
         }
 
-        cancelButton.setOnClickListener { dismiss()}
+        cancelButton.setOnClickListener { dismiss() }
+    }
+
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onViewStateRestored: called")
+
+        super.onViewStateRestored(savedInstanceState)
+        readValues()
+
+        firstDaySpinner.setSelection(firstDay - GregorianCalendar.SUNDAY)  // spinner values are zero-based
+
+        ignoreSeconds.progress = ignoreLessThan
+    }
+
+    private fun readValues() {
+        with(getDefaultSharedPreferences(context)) {
+            firstDay = getInt(SETTINGS_FIRST_DAY_OF_WEEK, defaultFirstDayOfWeek)
+            ignoreLessThan = getInt(SETTINGS_IGNORE_LESS_THAN, SETTINGS_DEFAULT_IGNORE_LESS_THAN)
+        }
+        Log.d(TAG, "Retrieving first day = $firstDay, ignoreLessThan = $ignoreLessThan")
+    }
+
+    private fun saveValues() {
+        val newFirstDay = firstDaySpinner.selectedItemPosition + GregorianCalendar.SUNDAY
+        val newIgnoreLessThan = ignoreSeconds.progress
+
+        Log.d(TAG, "Saving first day = $newFirstDay, ignore seconds = $newIgnoreLessThan")
+
+        with(getDefaultSharedPreferences(context).edit()) {
+            if (newFirstDay != firstDay) {
+                putInt(SETTINGS_FIRST_DAY_OF_WEEK, newFirstDay)
+            }
+            if (newIgnoreLessThan != ignoreLessThan) {
+                putInt(SETTINGS_IGNORE_LESS_THAN, newIgnoreLessThan)
+            }
+            apply()
+        }
     }
 }
